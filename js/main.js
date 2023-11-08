ddaaf73215e31e790e886e9749dfc899be6a420d
Akbar <akbarsafarov.as@gmail.com>
2023-11-08 21:30:14 +0500

    fixed re

--- js/main.js
+++ js/main.js
@@ -643,4 +643,28 @@ $(document).ready(function() {
 		    }
 		}
 	});
+
+	const quantityInput = document.getElementById("product-quantity");
+	const minusButton = document.querySelector(".product__amount-minus");
+	const plusButton = document.querySelector(".product__amount-plus");
+
+	minusButton.addEventListener("click", () => {
+		let currentValue = parseInt(quantityInput.value);
+
+		if (currentValue > 1) {
+		  	quantityInput.value = currentValue - 1;
+		}
+	});
+
+	plusButton.addEventListener("click", () => {
+		let currentValue = parseInt(quantityInput.value);
+		quantityInput.value = currentValue + 1;
+	});
+
+	quantityInput.addEventListener("input", () => {
+		let currentValue = parseInt(quantityInput.value);
+		if (isNaN(currentValue) || currentValue < 1) {
+		  	quantityInput.value = 1;
+		}
+	});
 });
